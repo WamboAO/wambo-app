3fe4aca3eedce4313b0f21bb8206b02f5389fb11
ruben sousa <rcsousa14@hotmail.com>
2021-09-05 22:56:17 +0100

    added first timers checker

--- lib/features/splash/domain/entities/authenticated_user_entity.dart
+++ lib/features/splash/domain/entities/authenticated_user_entity.dart
@@ -2,13 +2,16 @@ import 'package:equatable/equatable.dart';
 
 class AuthenticatedUserEntity extends Equatable {
   AuthenticatedUserEntity(
-      {required this.token, required this.userId, required this.refreshToken});
-  
+      {required this.token,
+      required this.userId,
+      required this.refreshToken,
+      required this.isFirstTime});
+
   final String token;
   final int userId;
+  final bool isFirstTime;
   final String refreshToken;
 
   @override
-  
-  List<Object?> get props => [token, userId, refreshToken];
+  List<Object?> get props => [token, userId, isFirstTime, refreshToken];
 }
