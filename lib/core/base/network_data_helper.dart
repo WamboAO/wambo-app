ee0db48f6314ad8307014aa337e116954f3497d0
ruben sousa <rcsousa14@hotmail.com>
2021-09-05 20:57:09 +0100

    network helper with exceptions

---
+++ lib/core/base/network_data_helper.dart
@@ -0,0 +1,28 @@
+import 'dart:async';
+import 'dart:io';
+import 'package:dartz/dartz.dart';
+import 'exception_helpers.dart';
+
+mixin NetworkDataHelper {
+  
+  Future requestFromDataSource(Future request) async {
+    try {
+      final result = await request;
+      return result;
+    } on SocketException {
+      return Left(FetchDataException('Sem conexão com a Internet'));
+    } on HttpException {
+      return Left(FetchDataException('O que procuras não existe'));
+    } on FormatException {
+      return Left(FetchDataException('Formato da resposta ruim'));
+    } on TimeoutException {
+      return Left(
+          FetchDataException('O pedido demorou muito. Tenta novamente'));
+    } on IOException {
+      return Left(
+          FetchDataException('Operação de input/output falhou ou interpretou'));
+    } on Exception  {
+      return Left(FetchDataException('Erro desconhecido'));
+    }  
+  }
+}
