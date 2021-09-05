98cf24e321f3ebc3d57bac566271b47eb34b7f74
ruben sousa <rcsousa14@hotmail.com>
2021-09-05 23:06:20 +0100

    added missing params for entity

--- test/features/splash/domain/usecases/get_authenticated_user_test.dart
+++ test/features/splash/domain/usecases/get_authenticated_user_test.dart
@@ -20,7 +20,7 @@ void main() {
     });
     final tParams = NoParams();
     final tAuth =
-        AuthenticatedUserEntity(token: "", userId: 0, refreshToken: "");
+        AuthenticatedUserEntity(token: "", userId: 0, refreshToken: "", isFirstTime: false);
     test('should get authenticated user from the repository', () async {
       when(() => repository.getAuthenticatedUserLocaly()).thenAnswer(
           (_) async => Right<ExceptionHelper, AuthenticatedUserEntity>(tAuth));
