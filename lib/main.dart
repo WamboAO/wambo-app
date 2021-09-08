2cb916ee0bf18cb60d9db9580020e26686d49e6e
ruben sousa <rcsousa14@hotmail.com>
2021-09-08 16:43:39 +0100

    added systemChrome

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:wambo/core/app/setup.locator.dart';
 import 'core/main/app_view.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIOverlays([SystemUiOverlay.bottom, SystemUiOverlay.top]);
   setupLocator();
   await dotenv.load(fileName: ".env");
   runApp(const MyApp());
