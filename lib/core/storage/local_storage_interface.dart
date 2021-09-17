e4630756ef46f2c83a481ca5b42de5c8f8d0b260
ruben sousa <rcsousa14@hotmail.com>
2021-09-17 19:11:23 +0100

    deleted items

--- lib/core/storage/local_storage_interface.dart
+++
@@ -1,6 +0,0 @@
-abstract class ILocalStorage {
-  Future get(String key);
-  Future clear();
-  Future delete(String key);
-  Future put(String key, dynamic value);
-}
--- lib/features/splash/data/datasources/authenticated_user_datasource.dart
+++
@@ -1,5 +0,0 @@
-import 'package:wambo/features/splash/data/models/authenticated_user_model.dart';
-
-abstract class IAuthenticatedUserDatasource {
-  Future<AuthenticatedUserModel> getAuthenticatedUserLocaly();
-}
