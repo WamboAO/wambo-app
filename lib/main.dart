5e8f697bc1a477b60883ca7d457133a11148c7a7
ruben.sousa <ruben.sousa@ideiasdinamicas.com>
2021-09-23 20:57:17 +0100

    added getAuth on init

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:wambo/core/app/locator.dart';
-import 'package:wambo/features/startup/presentation/view/startup/startup_view.dart';
+import 'package:wambo/app/locator.dart';
+import 'package:wambo/features/startup/presentation/view/startup_view.dart';
+import 'features/startup/presentation/services/startup_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setEnabledSystemUIOverlays(
       [SystemUiOverlay.bottom, SystemUiOverlay.top]);
 
-  await dotenv
-      .load(fileName: ".env")
-      .then((value) => setupLocator(enviroment: dotenv.env['ENVIROMENT']));
+  await dotenv.load(fileName: ".env").then((value) async {
+    setupLocator(enviroment: dotenv.env['ENVIROMENT']);
+    await locator<StartupService>().getAuthenticatedUserLocaly();
+  });
   
   runApp(const StartupView());
 }
+
+
