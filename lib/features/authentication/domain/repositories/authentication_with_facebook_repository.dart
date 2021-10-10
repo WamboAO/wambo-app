ff40a6400fee9fe0f7d11593d6b37db8f972c173
ruben.sousa <ruben.sousa@ideiasdinamicas.com>
2021-10-10 17:53:58 +0100

    deleted facebook repo

--- lib/features/authentication/domain/repositories/authentication_with_facebook_repository.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dartz/dartz.dart';
-import 'package:wambo/core/errors/failures.dart';
-import 'package:wambo/features/startup/domain/entities/authenticated_user_entity.dart';
-
-abstract class AuthenticationWithFacebookRepository {
-  Future<Either<Failure, AuthenticatedUserEntity>> loginWithFacebook();
-}
