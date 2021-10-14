cd27f7115190742316ef7b855fd1c7d131b6e4bf
ruben.sousa <ruben.sousa@ideiasdinamicas.com>
2021-10-14 22:59:58 +0100

    changed or deleted

--- lib/features/authentication/data/datasources/authentication_with_social_datasource.dart
+++
@@ -1,6 +0,0 @@
-import 'package:wambo/core/utils/enums.dart';
-import 'package:wambo/features/authentication/data/models/user_registration_credentials_model.dart';
-
-abstract class IAuthenticationWithSocialDatasource {
-  Future<UserRegistrationCredentialsModel> loginWithSocial(Social params);
-}
--- lib/features/authentication/domain/repositories/authenticate_user_repository.dart
+++
@@ -1,9 +0,0 @@
-import 'package:dartz/dartz.dart';
-
-import 'package:wambo/core/errors/failures.dart';
-import 'package:wambo/features/startup/domain/entities/authenticated_user_entity.dart';
-
-abstract class IAuthenticateUserRepository {
-  Future<Either<Failure, bool>> addAuthenticatedUserLocaly(
-      AuthenticatedUserEntity userAuth);
-}
