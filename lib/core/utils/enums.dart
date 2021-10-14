95c6f9c8f04c7f0995c5861453f20614452c74f4
ruben.sousa <ruben.sousa@ideiasdinamicas.com>
2021-10-14 23:01:34 +0100

    added layoutType

--- lib/core/utils/enums.dart
+++ lib/core/utils/enums.dart
@@ -1,5 +1,7 @@
 enum ConnectivityStatus { wifi, cellular, offiline }
 
-enum Status { loading, completed, error}
+enum Status { loading, completed, error }
 
-enum Social {facebook, google, apple}
+enum Social {facebook, google, apple }
+
+enum LayoutType {login, forgot, register }
