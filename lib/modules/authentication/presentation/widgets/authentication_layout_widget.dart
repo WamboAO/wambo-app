4dd076a6a2854d5d4f11dc36bcd2715141c3b96f
ruben.sousa <ruben.sousa@ideiasdinamicas.com>
2021-10-15 16:36:35 +0100

    minor changes to ui

--- lib/modules/authentication/presentation/widgets/authentication_layout_widget.dart
+++ lib/modules/authentication/presentation/widgets/authentication_layout_widget.dart
@@ -1,17 +1,40 @@
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:wambo/core/shared/ui/sizing.dart';
 import 'package:wambo/core/shared/ui/styles.dart';
+import 'package:wambo/core/shared/widgets/busy_button_widget.dart';
+import 'package:wambo/core/shared/widgets/text_field_widget.dart';
 import 'package:wambo/core/utils/enums.dart';
 import 'package:wambo/modules/authentication/presentation/widgets/contracts_widget.dart';
 
 class AuthenticationLayoutWidget extends StatelessWidget {
   const AuthenticationLayoutWidget(
-      {Key? key, required this.layoutType, this.onForgot})
+      {Key? key,
+      required this.layoutType,
+      required this.email,
+      required this.isBusy,
+      this.onPressed,
+      this.facebook,
+      this.password,
+      this.fname,
+      this.lname,
+      this.phone,
+      this.onForgot,
+      required this.emailFocus,
+      this.firstFocus,
+      this.lastfocus,
+      this.phoneFocus,
+      this.passFocus})
       : super(key: key);
   final LayoutType layoutType;
-  final Function? onForgot;
+  final bool isBusy;
+  final Function? onForgot, onPressed, facebook;
+  final TextEditingController email;
+  final FocusNode emailFocus;
+  final FocusNode? firstFocus, lastfocus, phoneFocus, passFocus;
+  final TextEditingController? fname, lname, phone, password;
 
   @override
   Widget build(BuildContext context) {
@@ -35,17 +58,66 @@ class AuthenticationLayoutWidget extends StatelessWidget {
                   fontWeight: FontWeight.bold, fontSize: 20),
               textAlign: TextAlign.center,
             ),
-            if (layoutType == LayoutType.register) ...[],
+            if (layoutType == LayoutType.register) ...[
+              SizedBox(
+                height: screenWidthPercentage(context, percentage: 0.03),
+              ),
+              const AutoSizeText(
+                "Criar uma conta para que possa comprar os seus produtos favoritos",
+                style: ktsMediumBodyText,
+                textAlign: TextAlign.center,
+              ),
+            ],
+            if (layoutType == LayoutType.forgot) ...[
+              SizedBox(
+                height: screenWidthPercentage(context, percentage: 0.03),
+              ),
+              const AutoSizeText(
+                "Digite o seu e-mail e enviaremos um link para redefinir a sua senha",
+                style: ktsMediumBodyText,
+                textAlign: TextAlign.center,
+              ),
+            ],
             SizedBox(
               height: screenWidthPercentage(context, percentage: 0.1),
             ),
             if (layoutType == LayoutType.login) ...[
               _LoginWidget(
+                onPressed: () => onPressed!(),
+                facebook: () => facebook!(),
+                isBusy: isBusy,
+                email: email,
+                emailFocus: emailFocus,
+                passFocus: passFocus!,
+                password: password!,
                 onForgot: () => onForgot!(),
               )
             ],
-            if (layoutType == LayoutType.register) ...[const _RegisterWidget()],
-            if (layoutType == LayoutType.forgot) ...[const _ForgotPasswordWidget()],
+            if (layoutType == LayoutType.register) ...[
+              _RegisterWidget(
+                facebook: () => facebook!(),
+                onPressed: () => onPressed!(),
+                isBusy: isBusy,
+                fname: fname!,
+                lname: lname!,
+                email: email,
+                phone: phone!,
+                password: password!,
+                emailFocus: emailFocus,
+                firstFocus: firstFocus!,
+                lastFocus: lastfocus!,
+                passFocus: passFocus!,
+                phoneFocus: phoneFocus!,
+              )
+            ],
+            if (layoutType == LayoutType.forgot) ...[
+              _ForgotPasswordWidget(
+                email: email,
+                emailFocus: emailFocus,
+                isBusy: isBusy,
+                onPressed: () => onPressed!(),
+              )
+            ],
           ],
         ),
       ),
@@ -54,22 +126,46 @@ class AuthenticationLayoutWidget extends StatelessWidget {
 }
 
 class _ForgotPasswordWidget extends StatelessWidget {
-  const _ForgotPasswordWidget({Key? key}) : super(key: key);
+  const _ForgotPasswordWidget(
+      {Key? key,
+      required this.email,
+      required this.emailFocus,
+      required this.isBusy,
+      required this.onPressed})
+      : super(key: key);
+  final TextEditingController email;
+  final FocusNode emailFocus;
+  final bool isBusy;
+  final Function onPressed;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const Placeholder(
-          fallbackWidth: double.infinity,
-          fallbackHeight: 45,
+        NewFormTextField(
+          onFieldSubmitted: (value) {
+            if (value.isNotEmpty) {
+              onPressed();
+            }
+          },
+          controller: email,
+          focus: emailFocus,
+          keyboardType: TextInputType.emailAddress,
+          icon: Icons.email_rounded,
+          text: "Email",
+          formater: FilteringTextInputFormatter.deny(
+            RegExp('[<>]'),
+          ),
         ),
         SizedBox(
           height: screenWidthPercentage(context, percentage: 0.1),
         ),
-        const Placeholder(
-          fallbackWidth: double.infinity,
-          fallbackHeight: 45,
+        BusyBtn(
+          busy: isBusy,
+          tap: () => onPressed(),
+          txtColor: kcWhite,
+          btnColor: Colors.black,
+          text: "Continuar",
         ),
       ],
     );
@@ -77,7 +173,26 @@ class _ForgotPasswordWidget extends StatelessWidget {
 }
 
 class _RegisterWidget extends StatelessWidget {
-  const _RegisterWidget({Key? key}) : super(key: key);
+  const _RegisterWidget(
+      {Key? key,
+      required this.facebook,
+      required this.fname,
+      required this.lname,
+      required this.email,
+      required this.isBusy,
+      required this.onPressed,
+      required this.password,
+      required this.phone,
+      required this.emailFocus,
+      required this.firstFocus,
+      required this.lastFocus,
+      required this.passFocus,
+      required this.phoneFocus})
+      : super(key: key);
+  final TextEditingController fname, lname, email, phone, password;
+  final FocusNode firstFocus, lastFocus, emailFocus, phoneFocus, passFocus;
+  final Function facebook, onPressed;
+  final bool isBusy;
 
   @override
   Widget build(BuildContext context) {
@@ -88,44 +203,84 @@ class _RegisterWidget extends StatelessWidget {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
-              const Expanded(
+              Expanded(
                 flex: 8,
-                child: Placeholder(
-                  fallbackWidth: double.infinity,
-                  fallbackHeight: 45,
+                child: NewFormTextField(
+                  textInputAction: TextInputAction.next,
+                  controller: fname,
+                  focus: firstFocus,
+                  keyboardType: TextInputType.text,
+                  icon: Icons.account_circle,
+                  text: "Nome",
+                  formater: FilteringTextInputFormatter.deny(
+                    RegExp('[<>]'),
+                  ),
                 ),
               ),
               const Spacer(),
-              const Expanded(
+              Expanded(
                 flex: 8,
-                child: Placeholder(
-                  fallbackWidth: double.infinity,
-                  fallbackHeight: 45,
+                child: NewFormTextField(
+                  textInputAction: TextInputAction.next,
+                  controller: lname,
+                  focus: lastFocus,
+                  keyboardType: TextInputType.text,
+                  icon: Icons.account_circle,
+                  text: "Último nome",
+                  formater: FilteringTextInputFormatter.deny(
+                    RegExp('[<>]'),
+                  ),
                 ),
-              )
+              ),
             ],
           ),
         ),
         SizedBox(
           height: screenWidthPercentage(context, percentage: 0.04),
         ),
-        const Placeholder(
-          fallbackWidth: double.infinity,
-          fallbackHeight: 45,
+        NewFormTextField(
+          textInputAction: TextInputAction.next,
+          controller: email,
+          focus: emailFocus,
+          keyboardType: TextInputType.emailAddress,
+          icon: Icons.email_rounded,
+          text: "Email",
+          formater: FilteringTextInputFormatter.deny(
+            RegExp('[<>]'),
+          ),
         ),
         SizedBox(
           height: screenWidthPercentage(context, percentage: 0.04),
         ),
-        const Placeholder(
-          fallbackWidth: double.infinity,
-          fallbackHeight: 45,
+        NewFormTextField(
+          textInputAction: TextInputAction.next,
+          focus: phoneFocus,
+          controller: phone,
+          keyboardType: TextInputType.phone,
+          icon: Icons.phone_rounded,
+          text: "Telefone",
+          formater: FilteringTextInputFormatter.deny(
+            RegExp('[<>]'),
+          ),
         ),
         SizedBox(
           height: screenWidthPercentage(context, percentage: 0.04),
         ),
-        const Placeholder(
-          fallbackWidth: double.infinity,
-          fallbackHeight: 45,
+        NewFormTextField(
+          onFieldSubmitted: (value) {
+            if (value.isNotEmpty) {
+              onPressed();
+            }
+          },
+          isPass: true,
+          controller: password,
+          focus: passFocus,
+          keyboardType: TextInputType.text,
+          icon: Icons.lock_rounded,
+          text: "Senha",
+          formater: FilteringTextInputFormatter.deny(
+            RegExp('[<>]'),
+          ),
         ),
         SizedBox(
           height: screenWidthPercentage(context, percentage: 0.04),
@@ -135,43 +290,86 @@ class _RegisterWidget extends StatelessWidget {
           terms: () => print("terms"),
           privacy: () => print("privacy"),
         ),
-        const Placeholder(
-          fallbackWidth: double.infinity,
-          fallbackHeight: 45,
+        BusyBtn(
+          busy: isBusy,
+          tap: () => onPressed(),
+          txtColor: kcWhite,
+          btnColor: Colors.black,
+          text: "Registar",
         ),
         SizedBox(
           height: screenWidthPercentage(context, percentage: 0.04),
         ),
-        const Placeholder(
-          fallbackWidth: double.infinity,
-          fallbackHeight: 45,
-        ),
+        ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                elevation: 5,
+                primary: const Color(0xFF4267B2),
+                fixedSize: Size.fromWidth(screenWidth(context)),
+                padding: const EdgeInsets.all(15)),
+            onPressed: () => facebook(),
+            child: AutoSizeText(
+              "Continua com Facebook",
+              style: ktsLargeBodyText.copyWith(color: Colors.white),
+            )),
       ],
     );
   }
 }
 
 class _LoginWidget extends StatelessWidget {
-  const _LoginWidget({Key? key, required this.onForgot}) : super(key: key);
+  const _LoginWidget(
+      {Key? key,
+      required this.onForgot,
+      required this.onPressed,
+      required this.facebook,
+      required this.isBusy,
+      required this.email,
+      required this.emailFocus,
+      required this.passFocus,
+      required this.password})
+      : super(key: key);
   final Function onForgot;
+  final TextEditingController email, password;
+  final FocusNode emailFocus, passFocus;
+  final Function onPressed, facebook;
+  final bool isBusy;
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        const Placeholder(
-          fallbackWidth: double.infinity,
-          fallbackHeight: 45,
+        NewFormTextField(
+          textInputAction: TextInputAction.next,
+          controller: email,
+          focus: emailFocus,
+          keyboardType: TextInputType.emailAddress,
+          icon: Icons.email_rounded,
+          text: "Email",
+          formater: FilteringTextInputFormatter.deny(
+            RegExp('[<>]'),
+          ),
         ),
         SizedBox(
           height: screenWidthPercentage(context, percentage: 0.04),
         ),
-        const Placeholder(
-          fallbackWidth: double.infinity,
-          fallbackHeight: 45,
+        NewFormTextField(
+          onFieldSubmitted: (value) {
+            if (value.isNotEmpty) {
+              onPressed();
+            }
+          },
+          isPass: true,
+          controller: password,
+          focus: passFocus,
+          keyboardType: TextInputType.text,
+          icon: Icons.lock_rounded,
+          text: "Senha",
+          formater: FilteringTextInputFormatter.deny(
+            RegExp('[<>]'),
+          ),
         ),
         SizedBox(
-          height: screenWidthPercentage(context, percentage: 0.035),
+          height: screenWidthPercentage(context, percentage: 0.05),
         ),
         AutoSizeText.rich(
           TextSpan(
@@ -180,38 +378,38 @@ class _LoginWidget extends StatelessWidget {
               TextSpan(
                   text: 'Esqueceu a Senha?',
                   style: ktsLargeBodyText.copyWith(
-                      fontWeight: FontWeight.bold, color: kcPrimary),
+                      fontWeight: FontWeight.w500,
+                      color: kcPrimary,
+                      decoration: TextDecoration.underline),
                   recognizer: TapGestureRecognizer()..onTap = () => onForgot()),
             ],
           ),
           textAlign: TextAlign.center,
         ),
         SizedBox(
-          height: screenWidthPercentage(context, percentage: 0.035),
+          height: screenWidthPercentage(context, percentage: 0.05),
         ),
-        const Placeholder(
-          fallbackWidth: double.infinity,
-          fallbackHeight: 45,
-        ),
-        SizedBox(
-          height: screenWidthPercentage(context, percentage: 0.1),
-        ),
-        AutoSizeText("Sem uma conta?",
-            style: ktsLargeBodyText.copyWith(fontWeight: FontWeight.bold)),
-        SizedBox(
-          height: screenWidthPercentage(context, percentage: 0.02),
-        ),
-        const Placeholder(
-          fallbackWidth: double.infinity,
-          fallbackHeight: 45,
+        BusyBtn(
+          busy: isBusy,
+          tap: () => onPressed(),
+          txtColor: kcWhite,
+          btnColor: Colors.black,
+          text: "Log in",
         ),
         SizedBox(
           height: screenWidthPercentage(context, percentage: 0.04),
         ),
-        const Placeholder(
-          fallbackWidth: double.infinity,
-          fallbackHeight: 45,
-        ),
+        ElevatedButton(
+            style: ElevatedButton.styleFrom(
+                elevation: 5,
+                primary: const Color(0xFF4267B2),
+                fixedSize: Size.fromWidth(screenWidth(context)),
+                padding: const EdgeInsets.all(15)),
+            onPressed: () => facebook(),
+            child: AutoSizeText(
+              "Continua com Facebook",
+              style: ktsLargeBodyText.copyWith(color: Colors.white),
+            )),
       ],
     );
   }
