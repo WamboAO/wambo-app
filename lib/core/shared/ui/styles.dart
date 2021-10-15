bd5c7edfa7eb1fa5d947e5ad37af9dbdb4917700
ruben.sousa <ruben.sousa@ideiasdinamicas.com>
2021-10-15 14:09:00 +0100

    added more colors

--- lib/core/shared/ui/styles.dart
+++ lib/core/shared/ui/styles.dart
@@ -4,18 +4,14 @@ const Color kcPrimary = Color(0xff26458F);
 const Color kcSecondary = Color(0xffE81463);
 const Color kcIconDark = Color(0xFF3D3D3D);
 const Color kcWhite = Color(0xFFffffff);
+const Color kcIconLight = Color(0xFFBDBDBD);
+const Color kcIcon = Color(0xFFB9B9B9);
 //textstyle
-const TextStyle ktsMediumBodyText = TextStyle(
-  fontSize: kBodyTextSize + 2.0,
-  color: kcIconDark
-);
-const TextStyle ktsLargeBodyText = TextStyle(
-  fontSize: kBodyTextSize + 4.0,
-  color: kcIconDark
-);
-const TextStyle ktsSmallBodyText = TextStyle(
-  fontSize: kBodyTextSize,
-  color: kcIconDark
-);
+const TextStyle ktsMediumBodyText =
+    TextStyle(fontSize: kBodyTextSize + 2.0, color: kcIconDark);
+const TextStyle ktsLargeBodyText =
+    TextStyle(fontSize: kBodyTextSize + 4.0, color: kcIconDark);
+const TextStyle ktsSmallBodyText =
+    TextStyle(fontSize: kBodyTextSize, color: kcIconDark);
 //font sizing
 const double kBodyTextSize = 13.0;
