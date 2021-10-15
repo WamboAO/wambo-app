ecf19856767746dea701c3fbcf3bd8c246a63a3b
ruben.sousa <ruben.sousa@ideiasdinamicas.com>
2021-10-15 14:07:22 +0100

    added fake auth response

--- lib/core/remote/fake_remote_data.dart
+++ lib/core/remote/fake_remote_data.dart
@@ -2,9 +2,24 @@ import 'package:faker/faker.dart';
 
 class FakeRemoteData {
   final _faker = Faker();
-  Map<String, Object> get fakeAppConfigValues => {
+  Map<String, Object?> get fakeAppConfigValues => {
         'app_id': _faker.guid.guid(),
         'app_token': _faker.jwt.expired(),
         'show_logo': true
+      };
+
+  Map<String, Object?> get fakeAuthUserResponseValues => {
+        'token': {
+          'token': _faker.jwt.expired(),
+          'refresh_token': _faker.guid.guid()
+        },
+        'user': {
+          'user_id': 1,
+          'first_name': _faker.person.firstName(),
+          'last_name': _faker.person.lastName(),
+          'phone': _faker.phoneNumber.us(),
+          'email': _faker.internet.disposableEmail(),
+          'avatar': _faker.image.image(),
+        }
       };
 }
