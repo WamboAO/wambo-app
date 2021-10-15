e49004b9cdce6532cb79b791b4d54716d76d3d49
ruben.sousa <ruben.sousa@ideiasdinamicas.com>
2021-10-15 14:09:15 +0100

    created busy button

---
+++ lib/core/shared/widgets/busy_button_widget.dart
@@ -0,0 +1,85 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:wambo/core/shared/ui/styles.dart';
+
+class BusyBtn extends StatelessWidget {
+   const BusyBtn({
+    Key? key,
+    required this.tap,
+    this.busy = false,
+    required this.txtColor,
+    this.text,
+    this.icon,
+    required this.btnColor,
+  }) : super(key: key);
+
+  final Function tap;
+  final String? text;
+  final Icon? icon;
+  final bool busy;
+  final Color btnColor;
+  final Color txtColor;
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(4),
+        ),
+        primary: btnColor,
+        elevation: 4,
+      ),
+      onPressed: busy == true ? null : () => tap(),
+      child: !busy
+          ? Padding(
+              padding: const EdgeInsets.all(15),
+              child: icon != null
+                  ? Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Flexible(flex: 1, child: icon!),
+                        Expanded(
+                          flex: 5,
+                          child: Center(
+                            child: AutoSizeText(
+                              text!,
+                              style: TextStyle(
+                                  color: txtColor,
+                                  fontWeight: FontWeight.w500,
+                                  fontSize: 15),
+                            ),
+                          ),
+                        ),
+                        const Flexible(
+                            flex: 1,
+                            child: SizedBox(
+                              height: 10,
+                              width: 10,
+                            ))
+                      ],
+                    )
+                  : Center(
+                      child: AutoSizeText(
+                        text!,
+                        style: ktsLargeBodyText.copyWith(color: txtColor),
+                      ),
+                    ),
+            )
+          : Center(
+              child: Padding(
+                padding: const EdgeInsets.all(15.0),
+                child: SizedBox(
+                  height: 21,
+                  width: 21,
+                  child: CircularProgressIndicator(
+                    strokeWidth: 3,
+                    valueColor: AlwaysStoppedAnimation<Color>(txtColor),
+                  ),
+                ),
+              ),
+            ),
+    );
+  }
+}
