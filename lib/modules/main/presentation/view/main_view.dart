7baa7396063a1caca3da1e10620cacbec5925073
ruben.sousa <ruben.sousa@ideiasdinamicas.com>
2021-10-15 07:43:09 +0100

    created main view

---
+++ lib/modules/main/presentation/view/main_view.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:stacked/stacked.dart';
+import 'package:wambo/modules/main/presentation/view/main_view_model.dart';
+
+class MainView extends StatelessWidget {
+ const MainView({Key? key}) : super(key: key);
+
+ @override
+ Widget build(BuildContext context) {
+   return ViewModelBuilder<MainViewModel>.reactive(
+     builder: (context, model, child) {
+        return WillPopScope(
+          onWillPop:  () async => false,
+          child: const Scaffold(
+            body: Center(child: Text("mainview"),),
+          ),
+        );
+        },
+     viewModelBuilder: () => MainViewModel(),
+   );
+ }
+}
---
+++ lib/modules/main/presentation/view/main_view_model.dart
@@ -0,0 +1,14 @@
+import 'package:stacked/stacked.dart';
+import 'package:stacked_services/stacked_services.dart';
+import 'package:wambo/app/setup.logger.dart';
+
+
+class MainViewModel extends BaseViewModel {
+   MainViewModel(){
+   log.d('created');
+ }
+ final log = getLogger('MainViewModel');
+
+
+
+}
