8f2bf0b283a97fce0a9d7e6653354ce647f15d2c
ruben.sousa <ruben.sousa@ideiasdinamicas.com>
2021-10-19 16:41:24 +0100

    added appToken to header

--- lib/core/shared/helpers/api_header_helpers.dart
+++ lib/core/shared/helpers/api_header_helpers.dart
@@ -1,12 +1,19 @@
 class ApiHeaders {
-  setTokenHeaders(String token) => {
+  setTokenHeaders({required String token,required String appToken}) => {
         'Content-type': 'application/json',
         'Accept': 'application/json',
         'Authorization': 'Bearer $token',
+        'App-Configuration': appToken
       };
 
-  setHeaders() => {
+setHeaders() => {
         'Content-type': 'application/json',
         'Accept': 'application/json',
+      
+      };
+  setAuthHeaders(String appToken) => {
+        'Content-type': 'application/json',
+        'Accept': 'application/json',
+        'App-Configuration': appToken
       };
 }
