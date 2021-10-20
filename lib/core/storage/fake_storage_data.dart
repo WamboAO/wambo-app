d01a20cbaa8beff4e2b51c18067f73214c00d13b
ruben.sousa <ruben.sousa@ideiasdinamicas.com>
2021-10-20 13:41:03 +0100

    minor change

--- lib/core/storage/fake_storage_data.dart
+++ lib/core/storage/fake_storage_data.dart
@@ -3,14 +3,14 @@
 class FakeLocalData {
   final _faker = Faker();
 Map<String, Object> get fakeAuthValues => {
-      // "token": _faker.jwt.expired(),
-      // "refresh_token": _faker.guid.guid(),
-      // "user_id": 1,
-      // "first_name": _faker.person.firstName(),
-      // "last_name": _faker.person.lastName(),
-      // "email": _faker.internet.freeEmail(),
-      // "phone": _faker.phoneNumber.us(),
-      // "avatar": _faker.image.image(),
-      // 'app_token': _faker.jwt.expired(),
+      "token": _faker.jwt.expired(),
+      "refresh_token": _faker.guid.guid(),
+      "user_id": 1,
+      "first_name": _faker.person.firstName(),
+      "last_name": _faker.person.lastName(),
+      "email": _faker.internet.freeEmail(),
+      "phone": _faker.phoneNumber.us(),
+      "avatar": _faker.image.image(),
+      'app_token': _faker.jwt.expired(),
     };
 }
