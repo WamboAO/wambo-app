6553b9db7458b8433e213b14d11ad068bbadb71e
ruben.sousa <ruben.sousa@ideiasdinamicas.com>
2021-10-21 08:39:28 +0100

    removed unused imports

--- lib/modules/main/presentation/view/main_view_model.dart
+++ lib/modules/main/presentation/view/main_view_model.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:stacked_services/stacked_services.dart';
 import 'package:wambo/app/locator.dart';
--- lib/modules/startup/data/datasources/get_refresh_token_datasource_implementation.dart
+++ lib/modules/startup/data/datasources/get_refresh_token_datasource_implementation.dart
@@ -3,7 +3,6 @@ import 'package:wambo/core/shared/helpers/api_header_helpers.dart';
 import 'package:wambo/modules/authentication/data/models/authentication_user_response_model.dart';
 import 'package:wambo/modules/startup/data/datasources/get_refresh_token_datasource.dart';
 import 'package:wambo/modules/startup/domain/entities/authenticated_user_entity.dart';
-import 'package:wambo/modules/startup/data/models/authenticated_user_model.dart';
 
 class GetRefreshTokenDatasourceImplementation
     implements IGetRefreshTokenDatasource {
