6e1c25c5878b801d949c250d51833f8aa81a029f
ruben.sousa <ruben.sousa@ideiasdinamicas.com>
2021-11-12 22:06:02 +0100

    added more colors

--- lib/core/shared/ui/styles.dart
+++ lib/core/shared/ui/styles.dart
@@ -4,8 +4,9 @@ const Color kcPrimary = Color(0xff26458F);
 const Color kcSecondary = Color(0xffE81463);
 const Color kcIconDark = Color(0xFF3D3D3D);
 const Color kcWhite = Color(0xFFffffff);
-const Color kcIconLight = Color(0xFFBDBDBD);
+const Color kcIconLight = Color(0xFF8D8D8D);
 const Color kcIcon = Color(0xFFB9B9B9);
+const Color kcBottomNav = Color(0xFFffffff);
 const Color kcFacebook = Color(0xFF4267B2);
 //textstyle
 const TextStyle ktsMediumBodyText =
