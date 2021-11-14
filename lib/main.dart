7e7490d49d156621c7c10877aae28b404dcac785
ruben.sousa <ruben.sousa@ideiasdinamicas.com>
2021-11-14 17:36:01 +0100

    added dark icons android status

--- lib/main.dart
+++ lib/main.dart
@@ -12,49 +12,41 @@ import 'core/shared/hooks/permission_handler.dart';
 import 'modules/startup/presentation/services/get_authenticated_user_service.dart';
 
 Future<void> main() async {
-  
   await dotenv.load(fileName: ".env").then((value) async {
-          setupLocator(enviroment: dotenv.env['ENVIROMENT']);
-          await locator<GetAuthenticatedUserService>().getUser();
-          await locator<PermissionHandler>().requestPermission();
-        });
-  CatcherOptions debugOptions = CatcherOptions(SilentReportMode(), [
-    ConsoleHandler(
-      enableStackTrace: false,
-      enableDeviceParameters: false
-    ),
-    DiscordHandler(
-        "https://discordapp.com/api/webhooks/900402922058289242/XkdqibNEmQIGPEf8JR_dHZp1jsQYzMhHxBth6xfojCdMTzoQ0Tpr1ZTM0WmqgpyQ1sMb",
-        enableDeviceParameters: true,
-        enableApplicationParameters: true,
-        enableCustomParameters: true,
-        enableStackTrace: true,
-       ),
-    
-  ], );
+    setupLocator(enviroment: dotenv.env['ENVIROMENT']);
+    await locator<GetAuthenticatedUserService>().getUser();
+    await locator<PermissionHandler>().requestPermission();
+  });
+  CatcherOptions debugOptions = CatcherOptions(
+    SilentReportMode(),
+    [
+      ConsoleHandler(enableStackTrace: false, enableDeviceParameters: false),
+    ],
+  );
 
   // /// Release configuration. Same as above, but once user accepts dialog, user will be prompted to send email with crash to support.
   CatcherOptions releaseOptions = CatcherOptions(SilentReportMode(), [
-    CrashlyticsHandler(
-      printLogs: false
-    ),
+    CrashlyticsHandler(printLogs: false),
     DiscordHandler(
-        "https://discordapp.com/api/webhooks/900402922058289242/XkdqibNEmQIGPEf8JR_dHZp1jsQYzMhHxBth6xfojCdMTzoQ0Tpr1ZTM0WmqgpyQ1sMb",
-        enableDeviceParameters: true,
-        enableApplicationParameters: true,
-        enableCustomParameters: true,
-        enableStackTrace: true,
-       ),
+      "https://discordapp.com/api/webhooks/900402922058289242/XkdqibNEmQIGPEf8JR_dHZp1jsQYzMhHxBth6xfojCdMTzoQ0Tpr1ZTM0WmqgpyQ1sMb",
+      enableDeviceParameters: true,
+      enableApplicationParameters: true,
+      enableCustomParameters: true,
+      enableStackTrace: true,
+    ),
   ]);
-SystemChrome.setEnabledSystemUIMode(
-            SystemUiMode.manual, overlays: [SystemUiOverlay.bottom, SystemUiOverlay.top]);
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+      overlays: [SystemUiOverlay.bottom, SystemUiOverlay.top]);
+ SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle.dark.copyWith(
+  statusBarColor: Colors.transparent,
+  statusBarIconBrightness: Brightness.dark
+));
   Catcher(
       rootWidget: const StartupView(),
       debugConfig: debugOptions,
       runAppFunction: () async {
-    
-       // screen direction
-        
+        // screen direction
+
         await Firebase.initializeApp();
         //send all errors
         FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterError;
@@ -64,9 +56,7 @@ SystemChrome.setEnabledSystemUIMode(
                 SvgPicture.svgStringDecoder, 'assets/images/wambo-login.svg'),
             null);
         //set Env
-        
       },
       releaseConfig: releaseOptions,
       ensureInitialized: true);
 }
-
