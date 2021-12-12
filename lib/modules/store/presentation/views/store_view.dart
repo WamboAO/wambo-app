07887b99a84be78803773c6830bd655576587c70
ruben.sousa <ruben.sousa@ideiasdinamicas.com>
2021-12-12 13:49:45 +0100

    modify data source

--- lib/modules/store/presentation/views/store_view.dart
+++ lib/modules/store/presentation/views/store_view.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:stacked/stacked.dart';
 import 'package:wambo/core/shared/ui/sizing.dart';
-import 'package:wambo/core/utils/enums.dart';
 import 'package:wambo/modules/store/presentation/views/store_view_model.dart';
-import 'package:wambo/modules/store/presentation/widgets/app_bar_view.dart';
+import 'package:wambo/modules/store/presentation/widgets/reactive/app_bar_view.dart';
 
-class StoreView extends HookWidget {
+class StoreView extends StatelessWidget {
   const StoreView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final search = useTextEditingController();
-    final focus = useFocusNode();
+   
     return ViewModelBuilder<StoreViewModel>.reactive(
       builder: (context, model, child) {
         return Scaffold(
@@ -20,19 +17,15 @@ class StoreView extends HookWidget {
           appBar: PreferredSize(
               preferredSize: Size.fromHeight(
                   screenHeightPercentage(context, percentage: 0.09)),
-              child: AppbarView(
-                controller: search,
-                focus: focus,
-                onClear: ()=> model.onClear(),
-              )),
+              child: const AppbarView()),
           body: Column(
             children: [
-              if (!model.dataReady) ...[],
-              if (model.dataReady) ...[
-                if (model.data!.status == Status.loading) ...[],
-                if (model.data!.status == Status.completed) ...[],
-                if (model.data!.status == Status.error) ...[],
-              ]
+              if (!model.dataReady || model.isLoading) ...[],
+              
+               
+                if (model.isError) ...[],
+                if (model.isComplete) ...[],
+             
             ],
           ),
         );
--- lib/modules/store/presentation/views/store_view_model.dart
+++ lib/modules/store/presentation/views/store_view_model.dart
@@ -3,24 +3,24 @@ import 'package:stacked_services/stacked_services.dart';
 import 'package:wambo/app/locator.dart';
 import 'package:wambo/app/setup.logger.dart';
 import 'package:wambo/core/utils/data_state_response.dart';
+import 'package:wambo/core/utils/enums.dart';
 import 'package:wambo/modules/store/domain/entities/store_info_entity.dart';
 import 'package:wambo/modules/store/presentation/services/get_store_info_service.dart';
 
-
 class StoreViewModel extends StreamViewModel<ApiResponse<StoreInfoEntity>> {
+  StoreViewModel() {
+    log.d('created');
+  }
 
- StoreViewModel(){
-   log.d('created');
- }
+  final log = getLogger('StoreViewModel');
+  final _getStoreInfoService = locator<GetStoreInfoService>();
 
- final log = getLogger('StoreViewModel');
- final _getStoreInfoService = locator<GetStoreInfoService>();
-
+  bool get isError => dataReady && data!.status == Status.error;
+  bool get isLoading => dataReady && data!.status == Status.loading;
+  bool get isComplete => dataReady && data!.status == Status.completed;
+  StoreInfoEntity? get competition =>
+      dataReady && data!.status == Status.completed ? data!.data! : null;
   @override
-
-  Stream<ApiResponse<StoreInfoEntity>> get stream => _getStoreInfoService.dataStream;
-
-  onClear() {}
-
+  Stream<ApiResponse<StoreInfoEntity>> get stream =>
+      _getStoreInfoService.dataStream;
 }
-
