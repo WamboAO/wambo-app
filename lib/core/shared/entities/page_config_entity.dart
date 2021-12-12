73890f1371d6eadff0d7d8675ce3de2f034bbd29
ruben.sousa <ruben.sousa@ideiasdinamicas.com>
2021-12-12 13:49:55 +0100

    added new fields

--- lib/core/shared/entities/page_config_entity.dart
+++ lib/core/shared/entities/page_config_entity.dart
@@ -1,18 +1,23 @@
 import 'package:equatable/equatable.dart';
 
 class PageConfigEntity extends Equatable {
-   PageConfigEntity({
-    required this.perPage,
-    required this.page,
+  PageConfigEntity({
+    this.perPage,
+    this.page,
+    this.filter,
+    this.search,
+    this.order,
     required this.appToken,
     required this.token,
   });
-  final int perPage;
-  final int page;
+  final int? perPage;
+  final int? page;
+  final String? filter;
+  final String? search;
+  final String? order;
   final String appToken;
   final String token;
- 
 
   @override
-  List<Object?> get props => [perPage, page, appToken, token];
+  List<Object?> get props => [perPage, page, filter, search, order, appToken, token];
 }
