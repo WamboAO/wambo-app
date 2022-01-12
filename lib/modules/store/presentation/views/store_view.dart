39d9da0852b56457d6f795a22a527cc9ff613abc
ruben.sousa <ruben.sousa@ideiasdinamicas.com>
2022-01-12 14:07:00 +0100

    new widgets added

--- lib/modules/store/presentation/views/store_view.dart
+++ lib/modules/store/presentation/views/store_view.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
 import 'package:wambo/core/shared/ui/sizing.dart';
+import 'package:wambo/core/shared/ui/styles.dart';
 import 'package:wambo/modules/store/presentation/views/store_view_model.dart';
 import 'package:wambo/modules/store/presentation/widgets/normal/app_bar_view.dart';
+import 'package:wambo/modules/store/presentation/widgets/reactive/forYou/foryou_view.dart';
 import 'package:wambo/modules/store/presentation/widgets/reactive/info/store_info_view.dart';
+import 'package:wambo/modules/store/presentation/widgets/reactive/popular/popular_view.dart';
+import 'package:wambo/modules/store/presentation/widgets/reactive/products/products_view.dart';
 import 'package:wambo/modules/store/presentation/widgets/reactive/promotions/promotions_view.dart';
+import 'package:wambo/modules/store/presentation/widgets/reactive/recent/recent_view.dart';
 
 class StoreView extends StatelessWidget {
   const StoreView({Key? key}) : super(key: key);
@@ -15,19 +20,38 @@ class StoreView extends StatelessWidget {
       onModelReady: (model) => model.int(),
       builder: (context, model, child) {
         return Scaffold(
-            backgroundColor: const Color(0xFFF1F1F1),
+            backgroundColor: kcIconLight,
             appBar: PreferredSize(
                 preferredSize: Size.fromHeight(
                     screenHeightPercentage(context, percentage: 0.16)),
                 child: AppbarView(
                   search: () => model.goToSearch(),
-                  qrcode: () => model.goToQRcode(),
                   cart: () => model.goToCart(),
                 )),
             body: SingleChildScrollView(
               child: Column(children: [
                 const PromotionView(),
-                const StoreInfoView()
+                const StoreInfoView(),
+                Container(
+                  color: kcWhite,
+                  child: GridView.count(
+                    padding: const EdgeInsets.all(5),
+                    shrinkWrap: true,
+                    childAspectRatio: 1.35,
+                    physics: const NeverScrollableScrollPhysics(),
+                    crossAxisCount: 2,
+                    children: [
+                      const PopularView(),
+                      const RecentView(),
+                      const ForYouView(),
+                      const PopularView(),
+                    ],
+                  ),
+                ),
+                const ProductsView(),
+                SizedBox(
+                  height: screenHeightPercentage(context, percentage: 0.07),
+                )
               ]),
             ));
       },
--- lib/modules/store/presentation/views/store_view_model.dart
+++ lib/modules/store/presentation/views/store_view_model.dart
@@ -3,6 +3,7 @@ import 'package:stacked_services/stacked_services.dart';
 import 'package:wambo/app/locator.dart';
 import 'package:wambo/app/setup.logger.dart';
 import 'package:wambo/core/shared/entities/page_config_entity.dart';
+import 'package:wambo/core/utils/enums.dart';
 import 'package:wambo/modules/startup/presentation/services/get_authenticated_user_service.dart';
 import 'package:wambo/modules/store/presentation/services/get_categories_service.dart';
 import 'package:wambo/modules/store/presentation/services/get_products_service.dart';
@@ -17,7 +18,11 @@ class StoreViewModel extends BaseViewModel {
   final _getCategoriesService = locator<GetCategoriesService>();
   final _getStoreInfoService = locator<GetStoreInfoService>();
   final _getAuthenticatedUserService = locator<GetAuthenticatedUserService>();
+  final _getProductsService = locator<GetProductsService>();
   final _getPromosService = locator<GetPromoService>();
+  final _getPopularService = locator<GetPopularService>();
+  final _getRecentService = locator<GetRecentService>();
+  final _getForYouService = locator<GetForYouService>();
 
   Future int() async {
     await _getCategoriesService.getCategories(
@@ -26,15 +31,27 @@ class StoreViewModel extends BaseViewModel {
       page: 1,
       appToken: _getAuthenticatedUserService.currentUser!.appToken!,
     ));
-    await _getStoreInfoService.getStoreInfo(
+
+    await Future.wait([
+      _getStoreInfoService.getStoreInfo(
+          config: PageConfigEntity(
+        appToken: _getAuthenticatedUserService.currentUser!.appToken!,
+      )),
+      _getPromosService.getPromos(),
+      _getPopularService.getPopular(),
+      _getRecentService.getRecent(),
+      _getForYouService.getForYou()
+    ]);
+    await _getProductsService.getProducts(
         config: PageConfigEntity(
+      perPage: 15,
+      page: 1,
+      productType: ProductType.all,
       appToken: _getAuthenticatedUserService.currentUser!.appToken!,
     ));
-
-   await _getPromosService.getPromos();
   }
 
-  goToQRcode() {}
+
 
   goToCart() {}
 
