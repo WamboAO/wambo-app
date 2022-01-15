d833c0ff246b1d7acc998fa7c76994df8149a487
ruben.sousa <ruben.sousa@ideiasdinamicas.com>
2022-01-15 11:18:57 +0100

    minor change

--- lib/modules/search/presentation/services/search_service.dart
+++ lib/modules/search/presentation/services/search_service.dart
@@ -1,13 +1,16 @@
 import 'package:rxdart/rxdart.dart';
+import 'package:wambo/app/locator.dart';
 import 'package:wambo/core/shared/entities/page_config_entity.dart';
 import 'package:wambo/core/utils/data_state_response.dart';
 import 'package:wambo/modules/search/domain/entities/search_items_entity.dart';
 import 'package:wambo/modules/search/domain/usecases/search_usecase.dart';
+import 'package:wambo/modules/startup/presentation/services/get_authenticated_user_service.dart';
 
-class SearchService  {
+class SearchService {
   SearchService(this.usecase);
 
   final SearchUsecase usecase;
+  final _getAuthenticatedUserService = locator<GetAuthenticatedUserService>();
 
   final BehaviorSubject<ApiResponse<SearchItemsEntity>> _response =
       BehaviorSubject<ApiResponse<SearchItemsEntity>>.seeded(
@@ -17,18 +20,18 @@ class SearchService  {
 
   Sink<ApiResponse<SearchItemsEntity>> get dataSink => _response.sink;
 
-  Future search({required PageConfigEntity config}) async {
-    
-    final result = await usecase(config);
+  Future search(String search) async {
+    dataSink.add(ApiResponse.loading("loading"));
+    final result = await usecase(PageConfigEntity(
+      search: search,
+      appToken: _getAuthenticatedUserService.currentUser!.appToken!,
+    ));
 
     ApiResponse<SearchItemsEntity> response =
         result.fold((l) => ApiResponse.error('$l'), (r) {
       return ApiResponse.completed(r);
     });
-    
-    dataSink.add(response);
-   
-  }
 
-  
+    dataSink.add(response);
+  }
 }
