8436e53ae1fdf125e4f3321736e3a2f2e094e57f
ruben.sousa <ruben.sousa@ideiasdinamicas.com>
2022-02-04 17:49:18 +0100

    added favorite appbar

--- lib/modules/favorite/presentation/views/favorite_view.dart
+++ lib/modules/favorite/presentation/views/favorite_view.dart
@@ -1,5 +1,8 @@
+import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:stacked/stacked.dart';
+import 'package:wambo/core/shared/ui/sizing.dart';
+import 'package:wambo/core/shared/ui/styles.dart';
 
 import 'favorite_view_model.dart';
 
@@ -10,8 +13,22 @@ class FavoriteView extends StatelessWidget {
  Widget build(BuildContext context) {
    return ViewModelBuilder<FavoriteViewModel>.nonReactive(
      builder: (context, model, child) {
-        return  const Scaffold(
-          body: Center(child: Text("favorite"),),
+        return   Scaffold(
+          appBar: PreferredSize(
+              preferredSize: Size.fromHeight(
+                  screenHeightPercentage(context, percentage: 0.07)),
+              child: AppBar(
+                elevation: 0,
+                automaticallyImplyLeading: false,
+                backgroundColor: kcWhite,
+                centerTitle: true,
+                title: AutoSizeText(
+                  "Favoritos",
+                  style: ktsMediumBodyText.copyWith(color: Colors.black),
+                ),
+              ),
+            ),
+          body: const Center(child: Text("favorite"),),
         );
         },
      viewModelBuilder: () => FavoriteViewModel(),
